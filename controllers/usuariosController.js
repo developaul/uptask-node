e367ee1a4e865f01f61ecceaa1ed741bd9cff0f1
Paul Chávez <paulchavezromero20@gmail.com>
2021-01-19 16:47:15 -0500

    validation on user creation added

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -11,10 +11,23 @@ exports.crearCuenta = async (req, res) => {
     // Leer los datos
     const { email, password } = req.body;
 
-    await Usuarios.create({
-        email,
-        password
-    });
+    try {
 
-    res.redirect('/iniciar-sesion');
+        await Usuarios.create({
+            email,
+            password
+        });
+
+        res.redirect('/iniciar-sesion');
+    } catch (error) {
+        req.flash('error', error.errors.map(error => error.message));
+
+        res.render('crearCuenta', {
+            nombrePagina: 'Crear Cuenta en Uptask',
+            mensajes: req.flash(),
+            email,
+            password
+        });
+    }
+
 }
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 // helpers
 const helpers = require('./helpers');
@@ -28,18 +31,31 @@ app.use(express.static('public'));
 // Habilitar view Engine
 app.set('view engine', 'pug');
 
+// Habilitar bodyParser para leer datos del formulario
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Añadir vistas
 app.set('views', path.join(__dirname, './views'));
+
+// agregar flash messages
+app.use(flash());
+
+app.use(cookieParser());
+
+// Sessiones nos permiten navegar sobre distintas páginas
+app.use(session({
+    secret: 'supersecreto',
+    resave: false,
+    saveUninitialized: false
+}))
 
 // Pasar var dump a la aplicación
 app.use((req, res, next) => {
     // Estableciendo variables que estaran disponibles en toda la app
     res.locals.vardump = helpers.vardump;
+    res.locals.mensajes = req.flash();
     next();
 });
-
-// Habilitar bodyParser para leer datos del formulario
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
 app.use('/', routes());
